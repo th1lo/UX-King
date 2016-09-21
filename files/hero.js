eb19fc92b4efe17781e92b4cb842a5b32b36b265
th1lo <th1lo@Thilos-MacBook-Pro.local>
2016-09-21 17:21:06 +0200

    Update

--- files/hero.js
+++ files/hero.js
@@ -1,10 +1,8 @@
-$( document ).ready(function() {
-  var images = {
-    background: ["./assets/img/herobg.jpg", "https://media.giphy.com/media/oxsfuzJuJzCjm/giphy.gif" , "https://media.giphy.com/media/12pWOEeKbbfdio/giphy.gif", "https://media.giphy.com/media/YrHOrXFvW4Nk4/giphy.gif"],
-    text: ["UX & Webdesign Agentur aus Berlin","Make UX Great Again","We Can Fix Your Product Retention", "Better UX For Harambe"]
-  };
-  var random = Math.floor(Math.random() * images.background.length);
-  console.log(images.text[random]);
-  $('#hero').css({'background-image': 'url(' + images.background[random] + ')'});
-  $('.memo').html(images.text[random]+ '<span>.</span>');
-});
+var images = {
+  background: ["https://media.giphy.com/media/beyUZ1fJVWfyE/giphy.gif", "https://media.giphy.com/media/oxsfuzJuJzCjm/giphy.gif" , "https://media.giphy.com/media/12pWOEeKbbfdio/giphy.gif", "https://media.giphy.com/media/YrHOrXFvW4Nk4/giphy.gif", "http://i.imgur.com/o6yQo2r.gif","http://i.imgur.com/Yffu60C.gif"],
+  text: ["UX & Webdesign Agentur aus Berlin","Make UX Great Again","We Can Fix Your Product Retention", "Better UX For Harambe", "We Help You Create Innovative Products","Never Stop Improving"]
+};
+var random = Math.floor(Math.random() * images.background.length);
+console.log(images.text[random]);
+$('#hero').css({'background-image': 'url(' + images.background[random] + ')'});
+$('.memo').html(images.text[random]+ '<span>.</span>');
