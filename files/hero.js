d82d157c7e97f2edf126b2d667a7c3aa83764595
th1lo <th1lo@Thilos-MacBook-Pro.local>
2016-09-22 12:03:29 +0200

    Hero Update

--- files/hero.js
+++ files/hero.js
@@ -1,13 +1,16 @@
 var images = {
   background: ["https://media.giphy.com/media/beyUZ1fJVWfyE/giphy.gif", "https://media.giphy.com/media/oxsfuzJuJzCjm/giphy.gif" , "https://media.giphy.com/media/12pWOEeKbbfdio/giphy.gif", "https://media.giphy.com/media/YrHOrXFvW4Nk4/giphy.gif", "http://i.imgur.com/o6yQo2r.gif","http://i.imgur.com/Yffu60C.gif"],
-  text: ["UX & Webdesign Agentur aus Berlin","Make UX Great Again","We Can Fix Your Product Retention", "Better UX For Harambe", "We Help You Create Innovative Products","Never Stop Improving"]
+  text: ["UX & Webdesign Agentur aus Berlin","Make Your UX Great Again","We Can Fix Your Product Retention", "Better UX For Harambe", "We Help You Create Innovative Products","Never Stop Improving"]
 };
+
 window.setInterval(function(){
-  var random = Math.floor(Math.random() * images.background.length);
-  $('#hero').css({'background-image': 'url(' + images.background[random] + ')'});
-  $('.memo').html(images.text[random]+ '<span>.</span>');
+ var random = Math.floor(Math.random() * images.background.length);
+ $('#hero').css({'background-image': 'url(' + images.background[random] + ')'});
+ $('.memo').html(images.text[random]+ '<span class="dot">.</span>').textillate({ in: { effect: 'fadeIn' } });
 }, 5000);
+
 
 var random = Math.floor(Math.random() * images.background.length);
 $('#hero').css({'background-image': 'url(' + images.background[random] + ')'});
-$('.memo').html(images.text[random]+ '<span>.</span>');
+$('.memo').html(images.text[random]+ '<span class="dot">.</span>');
+$('.tlt').textillate({ in: { effect: 'fadeIn' } });
